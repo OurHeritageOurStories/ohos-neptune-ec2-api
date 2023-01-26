7a7fd046d93700a0c59a260da57e1a345cb30131
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2023-01-26 12:09:22 +0000

    Probably close to ready, uploading to check

--- helm-local/alt_load_k8s.sh
+++ helm-local/alt_load_k8s.sh
@@ -2,7 +2,8 @@ minikube start
 
 helm install kong kong --set service.port=5000
 helm install miiify miiify
+helm install blazegraph blazegraph
 
-sleep 300
+sleep 60
 
 kubectl port-forward svc/kong 5000:5000
