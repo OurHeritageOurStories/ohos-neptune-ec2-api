0d3cd4666296128cb02aa7806ab24990693ee7df
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2023-02-01 14:39:37 +0000

    Working k8s for test purposes

--- helm-local/alt_load_k8s.sh
+++ helm-local/alt_load_k8s.sh
@@ -3,7 +3,8 @@ minikube start
 helm install kong kong --set service.port=5000
 helm install miiify miiify
 helm install blazegraph blazegraph
-helm install http-echo http-echo
+#helm install http-echo http-echo
+helm install echotest echotest
 
 sleep 60
 
--- helm-local/unload_k8s.sh
+++ helm-local/unload_k8s.sh
@@ -6,5 +6,6 @@ helm uninstall react-frontend
 helm uninstall kong
 helm uninstall iiif-generator
 helm uninstall http-echo
+helm uninstall echotest
 
 minikube stop
