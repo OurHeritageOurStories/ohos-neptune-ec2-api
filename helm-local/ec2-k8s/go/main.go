dedddf635e4e0edf305a7718ffb9128458c824e6
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2023-02-22 16:49:55 +0000

    Adjustments to the ec2 goapi code for dockerisation

--- helm-local/ec2-k8s/go/main.go
+++ helm-local/ec2-k8s/go/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+/*
 func requestToNeptune(c *gin.Context) {
 	sparqlQuery := "query=select ?s ?p ?o where {?s ?p ?o} limit 10"
 	sparqlBuffer := bytes.NewBuffer([]byte(sparqlQuery))
@@ -34,4 +35,52 @@ func main() {
 	router.POST("/other", requestToNeptune)
 	router.GET("/helloTest", testActive)
 	router.Run("localhost:9000")
+}
+*/
+
+func requestToNeptune(c echo.Context) error {
+	// Get name
+	name := c.FormValue("name")
+	// Get avatar
+	avatar, err := c.FormFile("avatar")
+	if err != nil {
+		return err
+	}
+
+	// Source
+	src, err := avatar.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(avatar.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<b>Thank you! "+name+"</b>")
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, you've reached the Go API that lets you talk to the Neptune database. Well done!")
+	})
+
+	e.POST("/sparql", requestToNeptune)
+
+	e.Logger.Fatal(e.Start(":9000"))
+
 }
