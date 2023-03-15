ca6d428d4799af16904e955a76b6fae7dc123016
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2023-03-15 15:40:01 +0000

    Test suite works on aws dockercompose as well

--- go-api/main_test.go
+++ go-api/main_test.go
@@ -33,7 +33,7 @@ func TestContains(t *testing.T) {
 func TestDocker(t *testing.T) {
 	pool, err := dockertest.NewPool("")
 	require.NoError(t, err, "Not connected to docker")
-	resource, err := pool.Run("data-go-api", "latest", []string{})
+	resource, err := pool.Run("public.ecr.aws/j8z6n5u1/data-go-api", "latest", []string{})
 	require.NoError(t, err, "could not start container")
 	t.Cleanup(func() {
 		require.NoError(t, pool.Purge(resource), "failedToRemove")
@@ -58,6 +58,35 @@ func TestDocker(t *testing.T) {
 	require.NoError(t, err, "Didn't read the body")
 	require.Contains(t, string(body), "done", "not done after all")
 
+}
+
+func TestResponse(t *testing.T) {
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err, "Not connected to docker")
+	resource, err := pool.Run("public.ecr.aws/j8z6n5u1/data-go-api", "latest", []string{})
+	require.NoError(t, err, "could not start container")
+	t.Cleanup(func() {
+		require.NoError(t, pool.Purge(resource), "failedToRemove")
+	})
+	var resp *http.Response
+
+	err = pool.Retry(func() error {
+		//resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("5000/tcp"), "/api/"))
+		resp, err = http.Get("http://localhost:5000/api/movingImages?keyword=glasgow&page=1")
+		if err != nil {
+			t.Log("container still loading")
+			return err
+		}
+		return nil
+	})
+	require.NoError(t, err, "http error")
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode, "http status code")
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "Didn't read the body")
+	require.Contains(t, string(body), "178", "not the right count after all")
 }
 
 /*
