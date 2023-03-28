757dad7ce3a3d975c9906265c84720259af68733
Harshad Gupta <harshad.gupta@nationalarchives.gov.uk>
2023-03-28 16:45:52 +0100

    UI changes addition Enitity

--- go-api/main.go
+++ go-api/main.go
@@ -12,7 +12,7 @@ import (
 	"strconv"
 	"strings"
 
-	_ "github.com/OurHeritageOurStories/ohos-neptune-ec2-api/docs"
+	// _ "github.com/OurHeritageOurStories/ohos-neptune-ec2-api/docs"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
@@ -49,6 +49,35 @@ type resultsBindingCount struct {
 	Value    string `json: value`
 }
 
+//struct for results for title/topic/url/description in movingImages
+type movingImagesTitleTopicUrlDesc struct {
+	Head    RDFHeadResponse
+	Results TitleTopicUrlDescriptionBindingStruct
+}
+
+type TitleTopicUrlDescriptionBindingStruct struct {
+	Bindings []BindingsTitleTopicUrlDescription
+}
+
+type BindingsTitleTopicUrlDescription struct {
+	Identifier 	IdentifierReturnValues
+	Title       TitleTopicStructValues
+	Description TitleTopicStructValues
+	URL         URLReturnValues
+	Topics      TitleTopicStructValues
+}
+
+type URLReturnValues struct {
+	Datatype string `json:"datatype"`
+	Type     string `json:"type"`
+	Value    string `json:"value"`
+}
+
+type IdentifierReturnValues struct {
+	Type     string `json:"type"`
+	Value    string `json:"value"`
+}
+
 // struct for results for title/topic search
 
 type TitleTopicStruct struct {
@@ -73,18 +102,92 @@ type TitleTopicStructValues struct {
 // struct for returning a keyword search
 
 type keywordReturnStruct struct {
-	Id    KeywordStruct               `json:"id"`
-	Total int                         `json:"total"`
-	First string                      `json:"first"`
-	Prev  string                      `json:"prev"`
-	Next  string                      `json:"next"`
-	Last  string                      `json:"last"`
-	Items []BindingsResultsTitleTopic `json:"items"`
+	Id    KeywordStruct                      `json:"id"`
+	Total int                                `json:"total"`
+	First string                             `json:"first"`
+	Prev  string                             `json:"prev"`
+	Next  string                             `json:"next"`
+	Last  string                             `json:"last"`
+	Items []BindingsTitleTopicUrlDescription `json:"items"`
+}
+
+type EntityReturnStruct struct {
+	Items []BindingsTitleTopicUrlDescription `json:"items"`
 }
 
 type KeywordStruct struct {
 	Page    string `json:"page"`
 	Keyword string `json:"keyword"`
+}
+
+//Discovery held by all result
+type discoveryAllStruct struct {
+	CagalogueLevels       []DiscoveryCodeCount
+	ClosureStatuses       []DiscoveryCodeCount
+	Count                 int `json:"count"`
+	Departments           []DiscoveryCodeCount
+	HeldByReps            []DiscoveryCodeCount
+	NextBatchMark         string `json:"nextBatchMark"`
+	Records               []DiscoveryRecordDetails
+	ReferenceFirstLetters []DiscoveryCodeCount
+	Repositories          []DiscoveryCodeCount
+	Sources               []DiscoveryCodeCount
+	TaxonomySubject       []DiscoveryCodeCount
+	TimePeriods           []DiscoveryCodeCount
+	TitleFirstLetters     []DiscoveryCodeCount
+}
+
+type DiscoveryRecordDetails struct {
+	AdminHistory       string   `json:"adminHistory"`
+	AltName            string   `json:"altName"`
+	Arrangement        string   `json:"arrangement"`
+	CatalogueLevel     int      `json:"catalogueLevel"`
+	ClosureCode        string   `json:"closureCode"`
+	ClosureStatus      string   `json:"closureStatus"`
+	ClosureType        string   `json:"closureType"`
+	Content            string   `json:"content"`
+	Context            string   `json:"context"`
+	CorpBodies         []string `json:"corpBodies"`
+	CoveringDates      string   `json:"coveringDates"`
+	Department         string   `json:"department"`
+	Description        string   `json:"description"`
+	DocumentType       string   `json:"documentType"`
+	EndDate            string   `json:"endDate"`
+	FormerReferenceDep string   `json:"formerReferenceDep"`
+	FormerReferencePro string   `json:"formerReferencePro"`
+	HeldBy             []string `json:"heldBy"`
+	Id                 string   `json:"id"`
+	MapDesignation     string   `json:"mapDesignation"`
+	MapScale           string   `json:"mapScale"`
+	Note               string   `json:"note"`
+	NumEndDate         int      `json:"numEndDate"`
+	NumStartDate       int      `json:"numStartDate"`
+	OpeningDate        string   `json:"openingDate"`
+	PhysicalCondition  string   `json:"physicalCondition"`
+	Places             []string `json:"places"`
+	Reference          string   `json:"reference"`
+	Score              int      `json:"score"`
+	Source             string   `json:"source"`
+	StartDate          string   `json:"startDate"`
+	Taxonomies         []string `json:"taxonomies"`
+	Title              string   `json:"title"`
+	UrlParameters      string   `json:"urlParameters"`
+}
+
+type DiscoveryCodeCount struct {
+	Code  string `json:"code"`
+	Count int    `json:"count"`
+}
+
+// TODO this should have more than one return
+func buildMainSparqlQuery(keyword string, offset string) string {
+	titleTopicURLDescription := "prefix ns0: <http://id.loc.gov/ontologies/bibframe/> prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix ns1: <http://id.loc.gov/ontologies/bflc/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>select (?s as ?identifier) ?title ?url ?description (group_concat(?topic;separator=' ||| ')as ?topics) where {?s ns0:title _:title ._:title ns0:mainTitle ?title filter (regex(str(?title), '" + keyword + "', 'i')) .?s ns0:summary _:summary ._:summary rdfs:label ?description .?s ns0:subject _:subject ._:subject rdfs:label ?topic .?s ns0:hasInstance ?t .?t ns0:hasItem ?r .?r ns0:electronicLocator _:url ._:url rdf:value ?url .} group by ?title ?s ?description ?url order by ?title  OFFSET " + offset + " limit 10"
+	return titleTopicURLDescription
+}
+
+func buildEntityMainSparqlQuery(id string) string {
+	titleTopicURLDescription := "prefix ns0: <http://id.loc.gov/ontologies/bibframe/> prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix ns1: <http://id.loc.gov/ontologies/bflc/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> prefix sub: <http://example.org/(filmRef)"+id+"#Work> select (sub: as ?identifier) ?title ?url ?description (group_concat(?topic;separator=' ||| ')as ?topics) where {sub: ns0:title _:title ._:title ns0:mainTitle ?title .sub: ns0:summary _:summary ._:summary rdfs:label ?description .sub: ns0:subject _:subject ._:subject rdfs:label ?topic .sub: ns0:hasInstance ?t .?t ns0:hasItem ?r .?r ns0:electronicLocator _:url ._:url rdf:value ?url .} group by ?title ?description ?url order by ?title OFFSET 0 limit 10"
+	return titleTopicURLDescription
 }
 
 // StatusCheck godoc
@@ -162,11 +265,13 @@ func requestToNeptune(c echo.Context) error {
 // @Description Requests to TNA Discovery API
 // @Tags Discovery
 // @Produce json
-// @Success 200
+// @Param q query string true "string query"
+// @Param source query string true "string sourceArchives"
+// @Success 200 {object} discoveryAllStruct
 // @Router /discovery [get]
 func fetchDiscovery(c echo.Context) error {
-	keyword := c.Request().URL.Query().Get("keyword")
-	source := strings.ToUpper(c.Request().URL.Query().Get("source"))
+	keyword := c.Param("q")
+	source := c.Param("source")
 
 	if source == "" {
 		source = "ALL"
@@ -245,6 +350,7 @@ func movingImages(c echo.Context) error {
 
 	countParams := url.Values{}
 	countParams.Add("query", countQuery)
+	countParams.Add("format", "json")
 	countBody := strings.NewReader(countParams.Encode())
 
 	countReq, err := http.NewRequest("POST", "https://ohos-live-data-neptune.cluster-ro-c7ehmaoz3lrl.eu-west-2.neptune.amazonaws.com:8182/sparql", countBody)
@@ -291,27 +397,28 @@ func movingImages(c echo.Context) error {
 
 	//Now that we know the number of pages, we can fill in the various page options
 	jsonToReturn.Total = numberOfResults
-	jsonToReturn.First = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/mvingImages?keyword=" + keyword + "&page=1"
+	jsonToReturn.First = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/mvingImages?q=" + keyword + "&page=1"
 	if off == 1 {
-		jsonToReturn.Prev = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?keyword=" + keyword + "&page=1"
+		jsonToReturn.Prev = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?q=" + keyword + "&page=1"
 	} else {
-		jsonToReturn.Prev = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?keyword=" + keyword + "&page=" + strconv.Itoa(off-1)
+		jsonToReturn.Prev = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?q=" + keyword + "&page=" + strconv.Itoa(off-1)
 	}
 	if off == maxPages {
-		jsonToReturn.Next = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?keyword=" + keyword + "&page=" + strconv.Itoa(maxPages)
+		jsonToReturn.Next = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?q=" + keyword + "&page=" + strconv.Itoa(maxPages)
 	} else {
-		jsonToReturn.Next = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?keyword=" + keyword + "&page=" + strconv.Itoa(off+1)
+		jsonToReturn.Next = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?q=" + keyword + "&page=" + strconv.Itoa(off+1)
 	}
-	jsonToReturn.Last = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?keyword=" + keyword + "&page=" + strconv.Itoa(maxPages)
+	jsonToReturn.Last = "http://ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000/api/movingImages?q=" + keyword + "&page=" + strconv.Itoa(maxPages)
 
 	defer countResp.Body.Close()
 
 	//Now, we do the actual query
 
-	mainSearchQuery := "prefix ns0: <http://id.loc.gov/ontologies/bibframe/> prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> prefix xsd: <http://www.w3.org/2001/XMLSchema#> prefix ns1: <http://id.loc.gov/ontologies/bflc/> prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> select ?title (group_concat(?topic;separator=' ||| ')as ?topics) where {?s ns0:title _:title ._:title ns0:mainTitle ?title filter (regex(str(?title), '" + keyword + "', 'i')) .?s ns0:subject _:subject ._:subject rdfs:label ?topic .} group by ?title order by ?title OFFSET " + offset + " LIMIT 10"
+	mainSearchQuery := buildMainSparqlQuery(keyword, offset)
 
 	mainSearchParams := url.Values{}
 	mainSearchParams.Add("query", mainSearchQuery)
+	mainSearchParams.Add("format", "json")
 	mainSearchBody := strings.NewReader(mainSearchParams.Encode())
 
 	mainSearchReq, err := http.NewRequest("POST", "https://ohos-live-data-neptune.cluster-ro-c7ehmaoz3lrl.eu-west-2.neptune.amazonaws.com:8182/sparql", mainSearchBody)
@@ -332,7 +439,66 @@ func movingImages(c echo.Context) error {
 
 	// Map the main response to the struct
 
-	var mainResultStruct TitleTopicStruct
+	var mainResultStruct movingImagesTitleTopicUrlDesc
+
+	mainResultData, err := ioutil.ReadAll(mainSearchResp.Body)
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Something went wrong reading the main response.")
+	}
+
+	if err := json.Unmarshal(mainResultData, &mainResultStruct); err != nil {
+		return c.String(http.StatusInternalServerError, "Something went wrong working with the main results.")
+	}
+
+	jsonToReturn.Items = mainResultStruct.Results.Bindings
+
+	return c.JSONPretty(http.StatusOK, jsonToReturn, " ")
+}
+
+// Moving Images Entity godoc
+// @Summary Moving images get specific entity query
+// @Description Moving images get specific entity query
+// @Tags MovingImages Entity
+// @Param id query string true "string id"
+// @Produce json
+// @Success 200 {object} EntityReturnStruct
+// @Success 204
+// @Failure 400
+// @Failure 500
+// @Router /movingImagesEntity [get]
+func movingImagesEntity(c echo.Context) error {
+
+	var jsonToReturn EntityReturnStruct
+
+	id := c.Param("id")
+
+	mainSearchQuery := buildEntityMainSparqlQuery(id)
+
+	mainSearchParams := url.Values{}
+	mainSearchParams.Add("query", mainSearchQuery)
+	mainSearchParams.Add("format", "json")
+	mainSearchBody := strings.NewReader(mainSearchParams.Encode())
+
+	mainSearchReq, err := http.NewRequest("POST", "https://ohos-live-data-neptune.cluster-ro-c7ehmaoz3lrl.eu-west-2.neptune.amazonaws.com:8182/sparql", mainSearchBody)
+
+	if err != nil {
+		log.Fatal(err)
+		return c.String(http.StatusInternalServerError, "Something went wrong sending the main request to the database.")
+	}
+
+	mainSearchReq.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	mainSearchResp, err := http.DefaultClient.Do(mainSearchReq)
+
+	if err != nil {
+		log.Fatal(err)
+		return c.String(http.StatusInternalServerError, "Something went wrong getting the main result from the database.")
+	}
+
+	// Map the main response to the struct
+
+	var mainResultStruct movingImagesTitleTopicUrlDesc
 
 	mainResultData, err := ioutil.ReadAll(mainSearchResp.Body)
 
@@ -350,12 +516,14 @@ func movingImages(c echo.Context) error {
 }
 
 // @title OHOS api
-// @version 1.0
+// @version 1.0.1
 // @description OHOS api
 // @termsOfService http://swagger.io/terms/
 // @contact.name The National Archives
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host ec2-13-40-156-226.eu-west-2.compute.amazonaws.com:5000
+// @BasePath /api
 func main() {
 
 	e := echo.New()
@@ -374,6 +542,8 @@ func main() {
 	e.GET("/discovery", fetchDiscovery)
 
 	e.GET("/movingImages", movingImages)
+
+	e.GET("/movingImages/entity/:id", movingImagesEntity)
 
 	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
