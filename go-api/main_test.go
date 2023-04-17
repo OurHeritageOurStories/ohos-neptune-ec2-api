e70e69e8ae9091af94c434d7bef17348616a2cf0
AndrewBewseyTNA <andrew.bewsey@nationalarchives.gov.uk>
2023-04-17 13:35:17 +0100

    Updates to test suite for env endpoints

--- go-api/main_test.go
+++ go-api/main_test.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/AndrewBewseyTNA/echo/v4"
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAPI(t *testing.T) {
-	currentEnv := os.Getenv("CURRENT_ENV")
-	err := godotenv.Load(currentEnv + ".env")
-	if err != nil {
-		log.Fatal("GoDotEnv didn't load properly")
-	}
 	welcomeString := os.Getenv("WELCOME_STRING")
 	url := "/"
 	e := echo.New()
@@ -42,15 +35,11 @@ func TestAPI(t *testing.T) {
 }
 
 func TestEntity(t *testing.T) {
-	currentEnv := os.Getenv("CURRENT_ENV")
-	err := godotenv.Load(currentEnv + ".env")
-	if err != nil {
-		log.Fatal("GoDotEnv didn't load properly")
-	}
 	neptuneUrl := os.Getenv("NEPTUNE_URL")
 	neptunePort := os.Getenv("NEPTUNE_PORT")
+	movingImagesEntityEndpoint := os.Getenv("MOVING_IMAGES_ENTITY_ENDPOINT")
 	neptuneFullSparqlUrl := neptuneUrl + ":" + neptunePort + "/sparql"
-	url := "/moving-images-ent/entity/(filmRef)5385"
+	url := "/" + movingImagesEntityEndpoint + "/entity/(filmRef)5385"
 	e := echo.New()
 	req, err := http.NewRequest(http.MethodPost, url, nil)
 	req.Header.Set("Content-Type", "application/json")
@@ -103,7 +92,7 @@ func TestEntity(t *testing.T) {
 	})
 
 	t.Run("should get error for missing id", func(t *testing.T) {
-		url := "moving-images-ent/"
+		url := movingImagesEntityEndpoint + "/"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -130,15 +119,11 @@ func TestEntity(t *testing.T) {
 }
 
 func TestDiscovery(t *testing.T) {
-	currentEnv := os.Getenv("CURRENT_ENV")
-	err := godotenv.Load(currentEnv + ".env")
-	if err != nil {
-		log.Fatal("GoDotEnv didn't load properly")
-	}
 	discoveryAPIurl := os.Getenv("DISCOVERY_API")
+	discoveryEndpoint := os.Getenv("DISCOVERY_ENDPOINT")
 
 	t.Run("should get records from all archives", func(t *testing.T) {
-		url := "discovery"
+		url := discoveryEndpoint
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -158,7 +143,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from all archives", func(t *testing.T) {
-		url := "discovery?source=ALL"
+		url := discoveryEndpoint + "?source=ALL"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -178,7 +163,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from other archives", func(t *testing.T) {
-		url := "discovery?source=OTH"
+		url := discoveryEndpoint + "?source=OTH"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -198,7 +183,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from TNA", func(t *testing.T) {
-		url := "discovery?source=TNA"
+		url := discoveryEndpoint + "?source=TNA"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -219,7 +204,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from all archives for edinburgh tram", func(t *testing.T) {
-		url := "discovery?q=edinburgh%20tram"
+		url := discoveryEndpoint + "?q=edinburgh%20tram"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -251,7 +236,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from all archives for edinburgh tram", func(t *testing.T) {
-		url := "discovery?q=edinburgh%20tram&source=ALL"
+		url := discoveryEndpoint + "?q=edinburgh%20tram&source=ALL"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -283,7 +268,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from other archives for edinburgh tram", func(t *testing.T) {
-		url := "discovery?q=edinburgh%20tram&source=OTH"
+		url := discoveryEndpoint + "?q=edinburgh%20tram&source=OTH"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -310,7 +295,7 @@ func TestDiscovery(t *testing.T) {
 	})
 
 	t.Run("should get records from TNA for edinburgh tram", func(t *testing.T) {
-		url := "discovery?q=edinburgh%20tram&source=TNA"
+		url := discoveryEndpoint + "?q=edinburgh%20tram&source=TNA"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -338,18 +323,14 @@ func TestDiscovery(t *testing.T) {
 }
 
 func TestMovingImages(t *testing.T) {
-	currentEnv := os.Getenv("CURRENT_ENV")
-	err := godotenv.Load(currentEnv + ".env")
-	if err != nil {
-		log.Fatal("GoDotEnv didn't load properly")
-	}
 	ec2url := os.Getenv("EC2_URL")
 	ec2port := os.Getenv("EC2_PORT")
 	neptuneUrl := os.Getenv("NEPTUNE_URL")
 	neptunePort := os.Getenv("NEPTUNE_PORT")
+	movingImagesEndpoint := os.Getenv("MOVING_IMAGES_ENDPOINT")
 	neptuneFullSparqlUrl := neptuneUrl + ":" + neptunePort + "/sparql"
 	ec2fullurl := ec2url + ":" + ec2port
-	url := "moving-images?q=glasgow&page=1"
+	url := movingImagesEndpoint + "?q=glasgow&page=1"
 	e := echo.New()
 	req, err := http.NewRequest(http.MethodPost, url, nil)
 	req.Header.Set("Content-Type", "application/json")
@@ -363,14 +344,14 @@ func TestMovingImages(t *testing.T) {
 	defer res.Body.Close()
 
 	t.Run("should get data for Glasgow", func(t *testing.T) {
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, rec.Code)
 			assert.Contains(t, rec.Body.String(), "178", "not getting the right data")
 		}
 	})
 
 	t.Run("should get error for missing page param", func(t *testing.T) {
-		url := "moving-images?q=glasgow"
+		url := movingImagesEndpoint + "?q=glasgow"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -382,14 +363,14 @@ func TestMovingImages(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
 			assert.Contains(t, rec.Body.String(), "You need to provide both a keyword (as q) and a page number", "Not throwing error")
 		}
 	})
 
 	t.Run("should get error for missing query param", func(t *testing.T) {
-		url := "moving-images?page=1"
+		url := movingImagesEndpoint + "?page=1"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -401,14 +382,14 @@ func TestMovingImages(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
 			assert.Contains(t, rec.Body.String(), "You need to provide both a keyword (as q) and a page number", "Not throwing error")
 		}
 	})
 
 	t.Run("should get error for page provided as string", func(t *testing.T) {
-		url := "moving-images?q=glasgow&page=string"
+		url := movingImagesEndpoint + "?q=glasgow&page=string"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -420,14 +401,14 @@ func TestMovingImages(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
 			assert.Contains(t, rec.Body.String(), "Page needs to be selected as a number", "Not throwing error")
 		}
 	})
 
 	t.Run("should not get data for page greater than max page", func(t *testing.T) {
-		url := "moving-images?q=glasgow&page=2500"
+		url := movingImagesEndpoint + "?q=glasgow&page=2500"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -439,14 +420,14 @@ func TestMovingImages(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
 			assert.Contains(t, rec.Body.String(), `"items": []`, "Showing data for more than max page")
 		}
 	})
 
 	t.Run("should not get data for non-existing keyword", func(t *testing.T) {
-		url := "moving-images?q=harshad&page=1"
+		url := movingImagesEndpoint + "?q=harshad&page=1"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -458,28 +439,14 @@ func TestMovingImages(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "Getting result for non existing keyword")
 			assert.Contains(t, rec.Body.String(), "The search worked, there just aren't any results", "Showing data for non-existing keyword")
 		}
 	})
 
-}
-
-func TestFailOnPurpose(t *testing.T) {
-	currentEnv := os.Getenv("CURRENT_ENV")
-	err := godotenv.Load(currentEnv + ".env")
-	if err != nil {
-		log.Fatal("GoDotEnv didn't load properly")
-	}
-	ec2url := os.Getenv("EC2_URL")
-	ec2port := os.Getenv("EC2_PORT")
-	neptuneUrl := os.Getenv("NEPTUNE_URL")
-	neptunePort := os.Getenv("NEPTUNE_PORT")
-	neptuneFullSparqlUrl := neptuneUrl + ":" + neptunePort + "/sparql"
-	ec2fullurl := ec2url + ":" + ec2port
-	t.Run("should not get data for non-existing keyword", func(t *testing.T) {
-		url := "moving-images?q=harshad&page=1"
+	t.Run("should get data for existing keyword", func(t *testing.T) {
+		url := movingImagesEndpoint + "?q=glasgow&page=1"
 		e := echo.New()
 		req, err := http.NewRequest(http.MethodPost, url, nil)
 		req.Header.Set("Content-Type", "application/json")
@@ -491,7 +458,73 @@ func TestFailOnPurpose(t *testing.T) {
 
 		res := rec.Result()
 		defer res.Body.Close()
-		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl)(c)) {
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
+			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
+			assert.Contains(t, rec.Body.String(), "The search worked, there just aren't any results", "Not throwing error")
+		}
+	})
+
+	t.Run("should test no keyword provided", func(t *testing.T) {
+		url := movingImagesEndpoint + "?page=1"
+		e := echo.New()
+		req, err := http.NewRequest(http.MethodPost, url, nil)
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			t.Errorf("The request could not be created because of: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		res := rec.Result()
+		defer res.Body.Close()
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
+			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
+			assert.Contains(t, rec.Body.String(), "You need to provide both a keyword (as q) and a page number", "Not throwing error")
+		}
+	})
+
+	t.Run("should test to many keywords provided", func(t *testing.T) {
+		url := movingImagesEndpoint + "?q=glasgow&page=1&test=1"
+		e := echo.New()
+		req, err := http.NewRequest(http.MethodPost, url, nil)
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			t.Errorf("The request could not be created because of: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		res := rec.Result()
+		defer res.Body.Close()
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesendpoint)(c)) {
+			assert.Equal(t, http.StatusOK, res.StatusCode, "http status code")
+			assert.Contains(t, rec.Body.String(), "You need to provide both a keyword (as q) and a page number", "Not throwing error")
+		}
+	})
+}
+
+func TestFailOnPurpose(t *testing.T) {
+	ec2url := os.Getenv("EC2_URL")
+	ec2port := os.Getenv("EC2_PORT")
+	neptuneUrl := os.Getenv("NEPTUNE_URL")
+	neptunePort := os.Getenv("NEPTUNE_PORT")
+	movingImagesEndpoint := os.Getenv("MOVING_IMAGES_ENDPOINT")
+	neptuneFullSparqlUrl := neptuneUrl + ":" + neptunePort + "/sparql"
+	ec2fullurl := ec2url + ":" + ec2port
+	t.Run("should not get data for non-existing keyword", func(t *testing.T) {
+		url := movingImagesEndpoint + "?q=harshad&page=1"
+		e := echo.New()
+		req, err := http.NewRequest(http.MethodPost, url, nil)
+		req.Header.Set("Content-Type", "application/json")
+		if err != nil {
+			t.Errorf("The request could not be created because of: %v", err)
+		}
+		rec := httptest.NewRecorder()
+		c := e.NewContext(req, rec)
+
+		res := rec.Result()
+		defer res.Body.Close()
+		if assert.NoError(t, movingImages(ec2fullurl, neptuneFullSparqlUrl, movingImagesEndpoint)(c)) {
 			assert.Equal(t, http.StatusOK, res.StatusCode, "Getting result for non existing keyword")
 			assert.Contains(t, rec.Body.String(), "'items': []", "Not showing error")
 		}
